d9efdbc66d34c0a676656c6c0ca32862b39ae910
Sestain <sestain3850@gmail.com>
2023-06-08 16:39:40 +0300

    Small changes to text.
    Font spacing was lowered to 0.9 from 0.95.
    Changed NTSC-U 1 to NTSC-U and NTSC-U 2 to NTSC-U 2.0

--- main.c
+++ main.c
@@ -89,7 +89,7 @@ static void InitScreen() {
 
 	gsKit_fontm_upload(gsGlobal, gsFontM);
 
-	gsFontM->Spacing = 0.95f;
+	gsFontM->Spacing = 0.9f;
 }
 
 static void InitPS2() {
@@ -136,19 +136,19 @@ int main() {
 
             if (new_pad & PAD_CROSS) {
                 gamename = "Crash Twinsanity PAL"; // PAL
-                filename = "cdrom0:\\SLES_525.68;1"; 
+                filename = "cdrom0:\\SLES_525.68;1";
             }  
             else if (new_pad & PAD_CIRCLE) {
                 gamename = "Crash Twinsanity NTSC-J"; // NTSC-J
-                filename = "cdrom0:\\SLPM_658.01;1"; 
+                filename = "cdrom0:\\SLPM_658.01;1";
             }
             else if (new_pad & PAD_SQUARE) {
-                gamename = "Crash Twinsanity NTSC-U 1"; // NTSC-U
-                filename = "cdrom0:\\SLUS_209.09;1"; 
+                gamename = "Crash Twinsanity NTSC-U"; // NTSC-U
+                filename = "cdrom0:\\SLUS_209.09;1";
             }
             else if (new_pad & PAD_TRIANGLE) {
-                gamename = "Crash Twinsanity NTSC-U 2"; // NTSC-U 2.0
-                filename = "cdrom0:\\SLUS_209.09_2;1"; 
+                gamename = "Crash Twinsanity NTSC-U 2.0"; // NTSC-U 2.0
+                filename = "cdrom0:\\SLUS_209.09_2;1";
             }
             if (new_pad & PAD_START) {
                 if (strcmp(gamename, "none") != 0 && executing != 2)
